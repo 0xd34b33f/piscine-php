1e94e1a31dcdef2e0a4811cef66ad451d8918d2f
rusmonkey <rusmonkey@users.noreply.github.com>
2019-05-03 22:14:53 +0300

    Crap. Tonight is raid night and I am already late.

--- day04/ex00/index.php
+++ day04/ex00/index.php
@@ -1,18 +1,17 @@
 <?php
 session_start();
-if ($_GET["submit"] == "ok" and $_GET["login"] and $_GET["passwd"]) {
+if ($_GET["submit"] == "OK" and $_GET["login"] and $_GET["passwd"]) {
     $_SESSION["login"] = $_GET["login"];
-    $_SESSION["pass"] = $_GET["passwd"];
-}
+    $_SESSION["pass"] = $_GET["passwd"];}
 ?>
 <html>
 <body>
 <form action="index.php" method="get">
-    login <input type="text" name="login" value=="<?php echo $_SESSION["login"]; ?>"/>l
+    login <input type="text" name="login" value="<?php echo $_SESSION["login"]; ?>"/>
     <br>
-    passwd<input type="text" name="passwd" value=<?php echo "\"" . $_SESSION["pass"] . "\""; ?>/>
+    passwd<input type="text" name="passwd" value="<?php echo $_SESSION["pass"]; ?>"/>
     <br>
-    <input type="submit" value="ok"/>
+    <input type="submit" name="submit" value="OK"/>
     <br>
 </form>
 </body>
