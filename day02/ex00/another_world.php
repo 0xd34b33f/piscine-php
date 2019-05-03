953f585fd3d198742893c6c814ce53c9341a608b
rusmonkey <rusmonkey@users.noreply.github.com>
2019-05-03 17:33:12 +0300

    :bug: fixed newline parsing

--- day02/ex00/another_world.php
+++ day02/ex00/another_world.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 if ($argc > 2) {
-    $arr = preg_split("@\s+@", $argv[1]);
+    $arr = preg_split("@[\t ]+@", $argv[1]);
     $arr = implode(" ", $arr);
     print $arr . "\n";
 }
