486332890ab7b6a7ddfb6073d7275064539d4e07
rusmonkey <rusmonkey@users.noreply.github.com>
2019-05-03 15:43:45 +0300

    :sparkles: Another day in php land
    get/post

--- day03/ex03/cookie_crisp.php
+++ day03/ex03/cookie_crisp.php
@@ -1,1 +1,19 @@
 <?php
+switch ($_GET["action"]) {
+    case "set":
+        {
+            setcookie($_GET["name"], $_GET["value"], time() + 60 * 60 * 24);
+            break;
+        }
+    case "get":
+        {
+            if ($_COOKIE[$_GET["name"]]) {
+                echo $_COOKIE[$_GET["name"]] . "\n";
+            }
+            break;
+        }
+    case "del":
+        {
+            setcookie($_GET["name"], 0, time() - 1);
+        }
+}
