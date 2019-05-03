db4e1c6d2327077931c6fb51e2a3d63b728d9300
rusmonkey <rusmonkey@users.noreply.github.com>
2019-05-04 00:01:40 +0300

    Reset error count between rows. herpderp

--- day04/ex01/create.php
+++ day04/ex01/create.php
@@ -1,13 +1,22 @@
 <!--echo, hash(), file_get_contents(), file_put_contents(),-->
 <!--serialize(), unserialize(), $_POST, file_exists(), mkdir()-->
 <?php
-print_r($_GET);
+function check_creds($string)
+{
+    if (hash("whirlpool", serialize($_GET["login"]) . ":" . serialize($_GET["passwd"])) === $string) {
+        return true;
+    } else
+        return false;
+}
+
+$pas = "";
+$us = "";
 if (!file_exists("./private/passwd")) {
     mkdir("./private");
     mkdir("./private/passwd");
 }
 $pas_path = "./private/passwd/passwords.crypt";
-if (!file_exists($pas_path)) {
-    if ($_GET["submit"] === "OK" and $_GET["passwd"] !== "")
-        file_put_contents($pas_path, hash("whirlpool", serialize($_GET["login"]) . ":" . serialize($_GET["passwd"])));
+if ($_GET["submit"] === "OK" and $_GET["passwd"] !== "") {
+
+    file_put_contents($pas_path, hash("whirlpool", serialize($_GET["login"]) . ":" . serialize($_GET["passwd"])));
 }
