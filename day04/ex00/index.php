e5c937b34e643207c76d736b7b146f9762a09ebd
rusmonkey <rusmonkey@users.noreply.github.com>
2019-05-03 18:22:17 +0300

    :sparkles: ejfoejf

--- day04/ex00/index.php
+++ day04/ex00/index.php
@@ -1,1 +1,19 @@
 <?php
+session_start();
+if ($_GET["submit"] == "ok" and $_GET["login"] and $_GET["passwd"]) {
+    $_SESSION["login"] = $_GET["login"];
+    $_SESSION["pass"] = $_GET["passwd"];
+}
+?>
+<html>
+<body>
+<form action="index.php" method="get">
+    login <input type="text" name="login" value=="<?php echo $_SESSION["login"]; ?>"/>l
+    <br>
+    passwd<input type="text" name="passwd" value=<?php echo "\"" . $_SESSION["pass"] . "\""; ?>/>
+    <br>
+    <input type="submit" value="ok"/>
+    <br>
+</form>
+</body>
+</html>
