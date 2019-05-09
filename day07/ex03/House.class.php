7a73e610556c23e43faf78d17d3f66239ef542ba
0xd34b33f <0xd34b33f@users.noreply.github.com>
2019-05-09 16:39:28 +0300

    That last commit message about silly mistakes pales in comparision to this one

--- day07/ex03/House.class.php
+++ day07/ex03/House.class.php
@@ -1,12 +1,11 @@
 <?php
 
-
 class House
 {
 	function introduce()
 	{
-		print $this->getHouseName().PHP_EOL;
-		print $this->getHouseMotto().PHP_EOL;
-		print $this->getHouseSeat().PHP_EOL;
+		print $this->getHouseName() . PHP_EOL;
+		print $this->getHouseMotto() . PHP_EOL;
+		print $this->getHouseSeat() . PHP_EOL;
 	}
 }
